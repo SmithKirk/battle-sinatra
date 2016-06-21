f0716972cada491741661df1d16a944721124ce4
Kirk Smith <kirks@hotmail.co.uk>
2016-06-21 16:25:32 +0100

    refactor post request to use a post-redirect loop

--- app.rb
+++ app.rb
@@ -1,16 +1,25 @@
 require 'sinatra/base'
 
 class MyApp < Sinatra::Base
+  enable :sessions
+
   get '/' do
-    'Hello MyApp!'
     erb :index
   end
 
   post '/names' do
-    @p1_name = params[:p1_name]
-    @p2_name = params[:p2_name]
+    session[:p1_name] = params[:p1_name]
+    session[:p2_name] = params[:p2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @p1_name = session[:p1_name]
+    @p2_name = session[:p2_name]
     erb :play
   end
+
+
 
 
 
